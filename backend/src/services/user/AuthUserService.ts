6078b542e0cbff9528450051da366896d52c506e
leninepestana <leninepestana@gmail.com>
2022-07-09 19:17:25 +0100

    JWT_SECRET

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string;
@@ -26,11 +27,28 @@ class AuthUserService {
       throw new Error("Username or password did not match");
     }
 
+    // Se estiver tuodo ok
     // Gerar um token JWT e devolver os dados do utilizador (id, name, email)
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
 
     console.log(email, password);
 
-    return { ok: true };
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    };
   }
 }
 
